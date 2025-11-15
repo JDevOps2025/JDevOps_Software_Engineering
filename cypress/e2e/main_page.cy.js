160689970765e036bde6ce9a18468eca95bcd1b9
JDevOps2025 <L00196546@atu.ie>
2025-11-15 22:24:05 +0000

    Cypress -> Functional Web Test

---
+++ cypress/e2e/main_page.cy.js
@@ -0,0 +1,12 @@
+/* Cypress Configuration File
+   Description: This file contains end-to-end tests for the homepage of the web application.
+   It uses Cypress to automate browser interactions and verify that the homepage loads correctly and displays the expected content.
+   Functional test to verify homepage renders the headline
+*/
+
+describe("Homepage", () => {
+  it("loads and shows the expected text", () => {
+    cy.visit("http://localhost:5000/");
+    cy.contains("Your web server is running"); // main message
+  });
+});
